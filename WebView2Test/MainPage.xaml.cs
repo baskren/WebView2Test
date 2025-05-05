47c5ee5e0c00ecd5a18ff75080448aac69cf21eb
Ben Askren <ben@buildcalc.com>
2025-05-05 11:38:08 -0400

    FIX : Not working in MacCatalyst and BrowserWasm

--- WebView2Test/MainPage.xaml.cs
+++ WebView2Test/MainPage.xaml.cs
@@ -3,7 +3,8 @@ namespace WebView2Test;
 
 public sealed partial class MainPage : Page
 {
-
+    string html = string.Empty;
+    
     public MainPage()
     {
         this.InitializeComponent();
@@ -11,6 +12,22 @@ public sealed partial class MainPage : Page
 #if DESKTOP
         myWebView.NavigationCompleted += (_, _) => myWebView.InvalidateArrange();
 #endif
+
+        if (GetType().Assembly.GetManifestResourceStream("WebView2Test.Resources.html5-test-page.html") is not
+            Stream stream)
+            return;
+
+        using (var reader = new StreamReader(stream))
+        {
+            html = reader.ReadToEnd();
+        }
+        
+        
+        Loaded += async (_, _) =>
+        {
+            await myWebView.EnsureCoreWebView2Async();
+            myWebView.NavigateToString(html);
+        };
     }
 
 }
